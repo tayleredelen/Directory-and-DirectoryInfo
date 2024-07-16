a749f19f805aa4786e8426414dc875a596930955
tayleredelen <taylerledelen@gmail.com>
2024-07-16 16:15:49 -0500

    chapter complete

--- Program.cs
+++ Program.cs
@@ -13,6 +13,17 @@ namespace CSharpFundamentals
             foreach (var file in files)
                 Console.WriteLine(file);
             
+            var directories = Directory.GetDirectories(@"c:\projects\CSharpFundamentals", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+
+
         }
     }
 }
